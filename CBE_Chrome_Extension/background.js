8be4f578e9181c0c1aaef33216424b8dbd8b2299
Jacob Whitten <whittej@students.wwu.edu>
2016-03-27 18:51:07 -0700

    temp before branching

---
+++ CBE_Chrome_Extension/background.js
@@ -0,0 +1,13 @@
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (tab.url == 'https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran') {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    }
+});
