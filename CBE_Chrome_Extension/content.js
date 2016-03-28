c455d8672544e3cbceaaa4be5bfea3ae71462922
Jacob Whitten <whittej@students.wwu.edu>
2016-03-28 16:44:50 -0700

    Updated Comments

--- CBE_Chrome_Extension/content.js
+++ CBE_Chrome_Extension/content.js
@@ -10,8 +10,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
   // First, validate the message's structure
   if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
     // Collect the necessary data 
-    // (For your specific requirements `document.querySelectorAll(...)`
-    //  should be equivalent to jquery's `$(...)`)
     var domInfo = {
       data: document.children[0].children[1].children[0].children[3].innerText
     };
