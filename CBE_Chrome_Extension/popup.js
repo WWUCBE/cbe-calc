f863c75a7cbf6237174ca668b3a1758e06d9e9c1
Jacob Whitten <jacob.r.whitten@gmail.com>
2016-03-27 20:05:17 -0700

    Changed popup.js so that elements of extension only show when current tab is on proper page

--- CBE_Chrome_Extension/popup.js
+++ CBE_Chrome_Extension/popup.js
@@ -24,10 +24,41 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+/*
+*
+*Functions to show/hide a div
+*
+*/
+function show(elementID) {
+    document.getElementById(elementID).style.display='block';
+}
+function hide(elementID) {
+    document.getElementById(elementID).style.display='none';
+}
+
+
+/*
+*
+*Redirects to unofficial transcript
+*
+*/
+window.addEventListener('load', function() {
+   var theAnchors = document.getElementsByClassName('wwuRedirect');
+   for(i=0, len=theAnchors.length; i<len; i++) {
+      theAnchors[i].addEventListener('click', function() {
+         chrome.tabs.create({url: "https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran"});
+      }, false);
+   }
+}, false);
+
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     if(url=="https://admin.wwu.edu/pls/wwis/wwskahst.WWU_ViewTran"){
-      renderStatus('On the correct page');
+      show('onPage');
+      hide('notOnPage');
+    }else{
+      show('notOnPage');
+      hide('onPage');
     };
   });
 });
