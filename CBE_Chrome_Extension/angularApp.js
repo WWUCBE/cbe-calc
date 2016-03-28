d6e4cf3653d77d195ee96e557abda5c73dfcf3b0
Jacob Whitten <whittej@students.wwu.edu>
2016-03-27 20:31:51 -0700

    angular controller updating page from DOM elements

--- CBE_Chrome_Extension/angularApp.js
+++ CBE_Chrome_Extension/angularApp.js
@@ -140,6 +140,36 @@ app.controller('MainCtrl', [
       $scope.classList.splice(index, 1);
       $scope.setGpa();
     };
-
+    $scope.updateDOMInfo = function(info){
+      document.getElementById('total').textContent   = info.total;
+      document.getElementById('inputs').textContent  = info.inputs;
+      document.getElementById('buttons').textContent = info.buttons;
+    }
   }
 ]);
+
+
+// Update the relevant fields with the new data
+function setDOMInfo(info) {
+  var scope = angular.element(document.getElementById("main")).scope();
+  scope.$apply(function(){
+    scope.updateDOMInfo(info);
+  });
+}
+
+// Once the DOM is ready...
+window.addEventListener('DOMContentLoaded', function () {
+  // ...query for the active tab...
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    // ...and send a request for the DOM info...
+    chrome.tabs.sendMessage(
+        tabs[0].id,
+        {from: 'popup', subject: 'DOMInfo'},
+        // ...also specifying a callback to be called 
+        //    from the receiving end (content script)
+        setDOMInfo);
+  });
+});
