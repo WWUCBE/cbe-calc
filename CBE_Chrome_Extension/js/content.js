7bfdadeaffbf8b8945286c046dfa78d7bac3315a
vankomw <vankomw@wwu.edu>
2017-10-26 13:06:51 -0700

    updated print function to have better formatting

--- CBE_Chrome_Extension/js/content.js
+++ CBE_Chrome_Extension/js/content.js
@@ -68,82 +68,17 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
   }
 });
 
-function formatForPrintOld(){
-  //Alert console that print format has been attempted
-  //alert("Message received");
-  console.log("Attempted to format");
-
-  //Erase page
-  document.body.innerHTML = '<br/><br/><br/><br/><br/><br/>';
-
-  //Append grade info to page (TEST)
-  var printDiv = document.createElement('printDiv');
-  printDiv.innerHTML = '';
-
-  //Create necessary variables
-  var classList = [];
-  var mode = 'UNSET';
-  var gpa = 0.0;
-  var credits = 0;
-
-  //Get mode and store it in variable
-  chrome.storage.sync.get('mode', function(result){
-    //Check if mode has been set
-    if(typeof(result.mode) != "undefined"){
-      //Set mode variable
-      if(result.mode === 'true'){
-        mode = 'CBE';
-      }else{
-        mode = 'MSCM';
-      }
-      console.debug("Mode: " + mode);
-      //Append mode to page
-      printDiv.innerHTML += "<b> PROGRAM: " + mode;
-    }
-  });
-
-  //Get gpa
-  chrome.storage.sync.get('gradeInfo', function(result){
-    //Check if mode has been set
-    if(typeof(result.gradeInfo) != "undefined"){
-      //Set mode variable
-      console.debug(result.gradeInfo);
-      //Append gradeInfo to page
-      printDiv.innerHTML += "<b><br/>GPA: " + result.gradeInfo[0];
-      printDiv.innerHTML += "<b><br/>CREDITS: " + result.gradeInfo[1];
-    }
-
-    //Add line
-    printDiv.innerHTML += "<br/><br/><br/><hr><b>Classes:</b></br></br>";
-  });
-
-  //Get classes and store them in variable
-  chrome.storage.sync.get('CBEclasses', function(result){
-    //Check to see if there are classes saved in storage
-    if((typeof(result.CBEclasses) != "undefined") && (result.CBEclasses.length > 0)){
-      //Set classList variable
-      classList = result.CBEclasses;
-      console.debug(classList);
-      //Loop to append each class to page
-      for(var i = 0 ; i < classList.length ; i++){
-        printDiv.innerHTML += "<b>" + classList[i].name + "</b><br/>GPA: " + classList[i].gpa + "<br/>CREDITS: " + classList[i].credits + "<br/><br/>";
-      }
-      printDiv.innerHTML += "<hr>";
-    }
-  });
-
-  //Add new div to page
-  document.body.appendChild(printDiv);
-}
-
 function formatForPrint(){
     
     console.log("running improved print function");
 
-    var printWindow = window.open('', 'PRINT', 'height=800,width=600');
+    var printWindow = window.open('', 'PRINT', 'height=900,width=700');
 
-    printWindow.document.write('<html><head><title>Unofficial GPA</title></head>');
-
+    printWindow.document.write('<html><head><link rel="stylesheet" href="../css/print.css" type="text/css" media="all">');
+    printWindow.document.write('<html><title>Unofficial GPA</title></head>');
+    printWindow.document.write('<img src="../resources/Western-logo-CBE.jpg" alt="WWU CBE logo" width="600"></br>');
+    
+    
     //Create necessary variables
     var classList = [];
     var mode = 'UNSET';
@@ -201,16 +136,16 @@ function formatForPrint(){
                     classList[i].gpa + "<br/>CREDITS: " + classList[i].credits +
                     "<br/><br/>");
             }
-        printWindow.document.write( "<hr>");
-		printWindow.document.write('</body></html>');
+          printWindow.document.write( "<hr>");
+	  printWindow.document.write('</body></html>');
 
-        printWindow.document.close();
-        printWindow.focus();
+          printWindow.document.close();
+          printWindow.focus();
 
 
-        //print the output
-        printWindow.print();
-		printWindow.close();
+          //print the output
+          setTimeout(function(){printWindow.print();},1000);
+          setTimeout(function(){printWindow.close();},1000);
         }
     });
 
